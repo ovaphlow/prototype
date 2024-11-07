390799cf90599b5b9882f2b746e56f638c2f6438
ovaphlow <ovaphlow@live.com>
2024-11-07 14:39:07 +0800

    prepare

--- mes-core-service/internal/order/order-repo.go
+++ mes-core-service/internal/order/order-repo.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"ovaphlow/mes/core/internal/infra"
+	"time"
 )
 
 type OrderRepo struct {
@@ -17,8 +18,22 @@ func NewOrderRepo(db *sql.DB) *OrderRepo {
 }
 
 func (r *OrderRepo) UpdateStatusById(status string, id string) error {
-	q := fmt.Sprintf(`UPDATE %s.order SET detail = jsonb_set(detail, '{status}', to_jsonb($1::text)) WHERE id = $2`, infra.SCHEMA_NAME)
-	_, err := r.db.Exec(q, status, id)
+	now := time.Now().Format("2006-01-02 15:04:05-07")
+	q := fmt.Sprintf(
+		`
+		UPDATE %s.order
+		SET detail = jsonb_set(detail, '{status}', to_jsonb($1::text)),
+			state = jsonb_set(state, '{updated_at}', to_jsonb($2::text))
+		WHERE id = $3`,
+		infra.SCHEMA_NAME,
+	)
+	stmt, err := r.db.Prepare(q)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(status, now, id)
 	if err != nil {
 		return err
 	}
