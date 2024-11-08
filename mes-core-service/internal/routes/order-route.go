30d7f3420f48e067563a5416081ff1ace4f6b2ef
ovaphlow <ovaphlow@live.com>
2024-11-08 13:54:27 +0800

    ..

--- mes-core-service/internal/routes/order-route.go
+++ mes-core-service/internal/routes/order-route.go
@@ -55,7 +55,6 @@ func SetupOrderRoutes(router *http.ServeMux, prefix string) {
 			http.Error(w, infra.MakeHTTPErrorResponse("请求错误", r), http.StatusBadRequest)
 			return
 		}
-		log.Println(data)
 		if err := orderService.Create(data); err != nil {
 			log.Println(err.Error())
 			http.Error(w, infra.MakeHTTPErrorResponse("服务器错误", r), http.StatusInternalServerError)
